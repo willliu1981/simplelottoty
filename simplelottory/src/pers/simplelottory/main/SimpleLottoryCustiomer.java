eb195912b3805de894c74abb456be0986fc30e5b
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-03-15 01:21:42 +0800

    SimpleLottoryCustomer add askCreateQuentity method

--- simplelottory/src/pers/simplelottory/main/SimpleLottoryCustiomer.java
+++ simplelottory/src/pers/simplelottory/main/SimpleLottoryCustiomer.java
@@ -4,34 +4,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import pers.simplelottory.control.Inputs;
 import pers.simplelottory.control.LottoryManager;
 import pers.simplelottory.control.LottoryManager.LottoryType;
 
 public class SimpleLottoryCustiomer {
-	
+
 	public static LottoryManager master_manager = new LottoryManager();
 	public static LottoryManager customer_manager = new LottoryManager();
-	
+
 	public static void main(String[] args) {
 		init();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String read;
-		System.out.println("App start...");
+		System.out.println("App SimpleLottoryCsutomer start...");
+		System.out.println(Inputs.tip());
 		try {
 			while (true) {
 				read = br.readLine();
-				
+				int num = askCreateQuantity(br);
+				System.out.println(""+num);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-	}
-	
-	
-	private static void init() {
-		
+
 	}
 
+	private static void init() {
+
+	}
+
+	private static int askCreateQuantity(BufferedReader br) throws IOException {
+		int num = 0;
+		System.out.print("Quqntity:");
+		while (true) {
+			String read = br.readLine();
+			/*
+			 * if illegal
+			 */
+			if (read == null || read.isEmpty() || !isNumeric(read)) {
+				System.out.println("Illegal inputs");
+				continue;
+			}
+			num = Integer.parseInt(read);
+			break;
+		}
+		return num;
+	}
+
+	private static boolean isNumeric(String data) {
+		if (data == null) {
+			return false;
+		}
+		try {
+			Integer.parseInt(data.trim());
+		} catch (NumberFormatException ex) {
+			System.out.println(ex.getMessage());
+			return false;
+		}
+		return true;
+	}
 }
---
+++ simplelottory/src/pers/simplelottory/test/Test7.java
@@ -0,0 +1,11 @@
+package pers.simplelottory.test;
+
+public class Test7 {
+
+	public static void main(String[] args) {
+		String s=" ";
+		
+		System.out.println(s.isEmpty());
+	}
+
+}
