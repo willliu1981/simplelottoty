a5dabcdf35aab4eb8b114a4933787587a5c83bc5
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-03-15 19:15:48 +0800

    fix App method createDefaultMasterManager and add createDefaultLottory

--- simplelottory/src/pers/simplelottory/control/App.java
+++ simplelottory/src/pers/simplelottory/control/App.java
@@ -7,31 +7,48 @@ import pers.simplelottory.model.Pool;
 public class App {
 	public static LottoryManager createDefaultMasterManager() {
 		LottoryManager master_manager = new LottoryManager();
-		Lottory bigLotto = new Lottory(new BigLottoDraw(), new Pool(49, 6), new Pool(1)) {
-			@Override
-			public String getPrimalInfo() {
-				StringBuilder sb = new StringBuilder();
-				this.getDrewNumbers(1).forEach(x -> sb.append(" " + x));
-				return super.getPrimalInfo() + "    special number:" + sb.toString();
-			}
-		};
-		Lottory lotto539 = new Lottory(new Pool(39, 5));
-		Lottory superlotto = new Lottory(new Pool(38, 6), new Pool(8, 1)) {
-			@Override
-			public String getPrimalInfo() {
-				StringBuilder sb = new StringBuilder();
-				this.getDrewNumbers(1).forEach(x -> sb.append(" " + x));
-				return super.getPrimalInfo() + "    Second Area number:" + sb.toString();
-			}
-		};
-		Lottory lotto24half = new Lottory(new Pool(24, 12));
-		Lottory bingo = new Lottory(new Pool(80, 20));
 
-		master_manager.addLottory(bigLotto, LottoryType.BigLotto);
-		master_manager.addLottory(lotto539, LottoryType.Lotto539);
-		master_manager.addLottory(superlotto, LottoryType.SuperLotto);
-		master_manager.addLottory(lotto24half, LottoryType.Lotto24half);
-		master_manager.addLottory(bingo, LottoryType.Bingo);
+		master_manager.addLottory(createDefaultLottory(LottoryType.BigLotto), LottoryType.BigLotto);
+		master_manager.addLottory(createDefaultLottory(LottoryType.Lotto539), LottoryType.Lotto539);
+		master_manager.addLottory(createDefaultLottory(LottoryType.SuperLotto), LottoryType.SuperLotto);
+		master_manager.addLottory(createDefaultLottory(LottoryType.Lotto24half), LottoryType.Lotto24half);
+		master_manager.addLottory(createDefaultLottory(LottoryType.Bingo), LottoryType.Bingo);
 		return master_manager;
+	}
+
+	public static Lottory createDefaultLottory(LottoryType type) {
+		Lottory lottory = null;
+		switch (type) {
+		case BigLotto:
+			lottory = new Lottory(new BigLottoDraw(), new Pool(49, 6), new Pool(1)) {
+				@Override
+				public String getPrimalInfo() {
+					StringBuilder sb = new StringBuilder();
+					this.getDrewNumbers(1).forEach(x -> sb.append(" " + x));
+					return super.getPrimalInfo() + "    special number:" + sb.toString();
+				}
+			};
+			break;
+		case Lotto539:
+			lottory = new Lottory(new Pool(39, 5));
+			break;
+		case SuperLotto:
+			lottory = new Lottory(new Pool(38, 6), new Pool(8, 1)) {
+				@Override
+				public String getPrimalInfo() {
+					StringBuilder sb = new StringBuilder();
+					this.getDrewNumbers(1).forEach(x -> sb.append(" " + x));
+					return super.getPrimalInfo() + "    Second Area number:" + sb.toString();
+				}
+			};
+			break;
+		case Lotto24half:
+			lottory = new Lottory(new Pool(24, 12));
+			break;
+		case Bingo:
+			lottory = new Lottory(new Pool(80, 20));
+			break;
+		}
+		return lottory;
 	}
 }
